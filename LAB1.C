0b5224ebafa9ac843703925a257cdd8e0ab4d60a
jishandankhara <136707738+jishandankhara@users.noreply.github.com>
2023-06-28 17:37:54 +0530

    Add files via upload

---
+++ LAB1.C
@@ -0,0 +1,15 @@
+//wrrite a program to print 1 to n using a while ioop.
+
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+int n;
+int a = 1;
+clrscr();
+ printf("enter any number:");
+ scanf("%d",&n);
+ while(a<=n){
+ printf("%d\n",a);
+ a++;}
+getch();}
---
+++ LAB2.C
@@ -0,0 +1,15 @@
+//6.1 Q2 write a program to print n to 1 using a while ioop.
+
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+int n;
+int a = 1;
+clrscr();
+ printf("enter any number:");
+ scanf("%d",&n);
+ while(n>=a){
+ printf("%d\n",n);
+ n--;}
+getch();}
